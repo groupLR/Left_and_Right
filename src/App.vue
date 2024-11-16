c00e9ee8624ab8a9f01588ec61e6aca57f18b892
33-Fang <33justworkplace@gmail.com>
2024-11-16 16:35:17 +0800

    feat: implement homepage video embed with RWD support

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,21 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
+import HomeView from './views/HomeView.vue';
 
 </script>
 
 <template>
   <header>
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-        留兩個分頁給大家參考
-      </nav>
-  </header>
+    <nav>
+      <!-- <RouterLink to="/">Home</RouterLink> -->
+      <!-- <RouterLink to="/about">About</RouterLink> -->
+    </nav>
 
-  <RouterView />
+  </header>
+  <main>
+    <HomeView />
+  </main>
+  <!-- <RouterView /> -->
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,5 +4,18 @@
 
 <template>
   <main>
+    <section id="promoteVideo" class="px-4 w-full	my-0 mx-auto md:w-[750px] lg:w-[970px] xl:w-[1170px]">
+      <div class="p-4 w-full">
+        <iframe class="w-full aspect-video "
+          src="https://www.youtube.com/embed/APEujcFMCxs?autoplay=1&loop=1&playlist=APEujcFMCxs&rel=1"
+          title="YouTube video player" frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+      </div>
+
+    </section>
   </main>
 </template>
+
+<style scoped>
+</style>
