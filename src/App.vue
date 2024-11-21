a9ce999a01da0c585773520715e9200d667d5c1d
Leon <6011212@gmail.com>
2024-11-21 16:24:07 +0800

    StoreInfo+Carousel 初版

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,8 @@
 import { RouterLink, RouterView } from 'vue-router'
 import HomeView from './views/HomeView.vue';
 import Footer from './components/Footer.vue';
-
-
+import Carousel2 from './components/Carousel2.vue';
+import StoreInfo4 from './components/StoreInfo4.vue';
 </script>
 
 <template>
@@ -31,11 +31,13 @@ import Footer from './components/Footer.vue';
       | 
       <RouterLink to="/products">products</RouterLink>
       |
-      <RouterLink to="/store-info">store-info</RouterLink>
+      <RouterLink to="/store-info">store-info</RouterLink> -->
     </nav>
   </header>
-  <RouterView />
-  <Footer />
+  <!-- <RouterView /> -->
+  <!-- <Carousel2/> -->
+  <StoreInfo4/>
+  <!-- <Footer /> -->
 </template>
 
 <style scoped></style>
