7f1fe8c9089ec6b3efdcac60fd1694bb59d43ae1
33-Fang <33justworkplace@gmail.com>
2024-11-22 21:57:14 +0800

    修復 product 相關檔案；修復 tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,5 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html',
-    "./src/**/*.{js,ts,jsx,tsx}"],
-  theme: {
-    extend: {},
   content: [
     "./index.html",
     "./src/**/*.{vue,js,ts,jsx,tsx}",
@@ -26,5 +22,4 @@ export default {
     },
   },
   plugins: [],
-}
 }
