843edbe6be28092c5609bd0df3baa0853cc19adb
cloverche <sunny40224@gmail.com>
2024-11-24 15:13:31 +0800

    遮罩告示

--- src/views/UsersEdit.vue
+++ src/views/UsersEdit.vue
@@ -10,7 +10,34 @@
                 <div class="bg-[#EEEEEE] p-2.5">
                     <p class="font-extrabold"><i class="fa-solid fa-star pr-2.5"></i>升級至VIP會員</p>
                     <p>24 個月內累積消費額達 NT$4000即可升級</p>
-                    <a class="text-blue-600">查看所有會員等級規則</a>
+                    <!-- 點擊 -->
+                    <input type="checkbox" name="" id="rule" class="checkbox">
+                    <label for="rule" class="text-blue-600 cursor-pointer">查看所有會員等級規則</label>
+                    <!-- 遮罩區塊 -->
+                    <div class="overlay fixed inset-0 z-50 flex items-center justify-center">
+                        <!-- 白色區塊 -->
+                        <div class="rule-content bg-white rounded-lg p-6 max-w-md w-[90%] relative z-10 flex justify-between lg:max-w-lg">
+                            <div>
+                                <div class="mb-2.5">
+                                    <h3 class="font-extrabold">Bonny & Read 飾品會員等級規則</h3>
+                                    <p class="font-extralight text-sm">會籍期效內若沒達成續會條件，將會自動降級</p>
+                                </div>
+                                <div class="mb-2.5">
+                                    <h3 class="font-extrabold">VIP 會員</h3>
+                                    <p class="font-extralight text-sm">本階升級條件 :24 個月內累積消費額達 NT$4000 即可升級</p>
+                                    <p class="font-extralight text-sm">有效期限 :永久有效</p>
+                                </div>
+                                <div>
+                                    <h3 class="font-extrabold">白金會員</h3>
+                                    <p class="font-extralight text-sm">本階升級條件 :24 個月內累積消費額達 NT$6000 即可升級</p>
+                                    <p class="font-extralight text-sm">有效期限 :永久有效</p>
+                                </div>
+                            </div>
+                            <label for="rule"class="text-gray-400 hover:text-gray-600 text-xl cursor-pointer">×</label>
+                        </div>
+                        <!-- 遮罩 -->
+                        <label for="rule" class="absolute inset-0 bg-black/50 cursor-pointer"></label>
+                    </div>
                 </div>
                 <div class="p-2.5">
                     <h2 class="font-extrabold"><i class="fa-solid fa-flag pr-2.5"></i>24 個月內累積消費額</h2>
@@ -19,7 +46,48 @@
             </div>
             <div class="block w-full my-10 leading-5 pl-[5px] lg:pl-0">
                 <h2 class="mb-4 font-extrabold"><i class="fa-solid fa-circle-user pr-2.5"></i>會員推薦優惠</h2>
-                <p class="text-base">推薦好友一起賺！領免費購物金: 複製並分享連結給朋友，推薦對象和你將獲得購物金。<a class="text-blue-600">活動規則說明</a></p>
+                <!-- 點擊 -->
+                <input type="checkbox" name="" id="activity" class="checkbox">
+                <label for="activity">推薦好友一起賺！領免費購物金: 複製並分享連結給朋友，推薦對象和你將獲得購物金。<p class="text-blue-600 cursor-pointer">活動規則說明</p></label>
+                <!-- 遮罩區塊 -->
+                <div class="overlay fixed inset-0 z-50 flex items-center justify-center">
+                    <!-- 白色區塊 -->
+                    <div class="rule-content bg-white rounded-lg p-8 max-w-lg w-[90%] relative z-10 flex justify-between lg:max-w-2xl">
+                        <div>
+                            <div class="mb-3">
+                                <h3 class="font-extrabold">活動規則說明</h3>
+                            </div>
+                            <div>
+                                <div class="font-extralight text-sm pb-2.5">
+                                    <h3 class="font-normal pb-1">【活動說明】</h3> 
+                                    <p class="pl-5">推薦人：被推薦人註冊後完成訂單（需滿$350），即可獲得$50購物金</p>
+                                    <p class="pl-5">被推薦人：使用推薦連結註冊成為會員，即可獲得$30購物金</p>
+                                </div>
+                                <div class="font-extralight text-sm pb-2.5">
+                                    <h3 class="font-normal pb-1">【使用規則】</h3>
+                                    <p class="pl-5">1-不限金額皆可使用</p>
+                                    <p class="pl-5">2-使用期限為領取後90天內（含領取當天）</p>
+                                    <p class="pl-5">3-Cosmic mansion韓國香氛、備品商品、指定系列3件5折、11元起商品不適用購物金</p>
+                                </div>
+                                <div class="font-extralight text-sm pb-2.5">
+                                    <h3 class="font-normal pb-1">【如何推薦好友】</h3>
+                                    <p class="pl-5">1-前往「 會員中心 > 個人資訊」，找到「會員推薦優惠」區塊，點按「複製連結」</p>
+                                    <p class="pl-5">2-需要先點按「產生推薦連結」，才會出現「推薦連結」及「複製連結」的按鈕</p>
+                                    <p class="pl-5">3-將連結分享給朋友</p>
+                                    <p class="pl-5">4-朋友成功註冊且第一筆訂單改為「已完成」&金額達350元，即可獲得購物金</p>
+                                </div>
+                                <div class="font-extralight text-sm">
+                                    <h3 class="font-normal pb-1">【新會員如何使用購物金】</h3>
+                                    <p class="pl-5">1-點按「會員推薦連結」後，點「註冊並領取」並註冊，即可獲得購物金</p>
+                                    <p class="pl-5">2-註冊成功後，畫面會彈出獲得購物金的通知</p>
+                                </div>
+                            </div>
+                        </div>
+                        <label for="activity"class="text-gray-400 hover:text-gray-600 text-xl cursor-pointer">×</label>
+                    </div>
+                    <!-- 遮罩 -->
+                    <label for="activity" class="absolute inset-0 bg-black/50 cursor-pointer"></label>
+                </div>
                 <p>https://www.bonnyread.com.tw/?sl-mref=Q6Bto<a class="text-blue-600"><i class="fa-solid fa-link"></i>複製連結</a></p>
             </div>
             <div class="grid w-full grid-cols-1 gap-5 justify-between lg:grid-cols-2">
@@ -181,23 +249,45 @@
         </div>
     </div>
 </template>
+
 <style scoped>
+    .checkbox { 
+        display: none; 
+    }
+    .checkbox:not(:checked) ~ .overlay { 
+        display: none; 
+    }
+    .rule-content {
+        animation:SlideIn 0.3s ease-out;
+    }
+
+    /* 模態框動畫 */
+    @keyframes SlideIn {
+        from {
+            opacity: 0;
+            transform: scale(0.8)
+        }
+        to {
+            opacity: 1;
+            transform: scale(1);
+        }
+    }
     .container{ 
-        max-width: 1438px;
+    max-width: 1438px;
     }
     .border {
-        border: 1px solid #EEEEEE;
+    border: 1px solid #EEEEEE;
     }
     input,select{
-        border: 1px solid #EEEEEE;
-        padding-inline: 8px;
+    border: 1px solid #EEEEEE;
+    padding-inline: 8px;
     }
     .user{
-        width: 100%;
-        display: flex;
-        gap:10px;
-        align-items: center;
-        font-weight: 800;
+    width: 100%;
+    display: flex;
+    gap:10px;
+    align-items: center;
+    font-weight: 800;
     }
     .name,
     .email,
@@ -210,91 +300,91 @@
     .password,
     .third-party-registration,
     .address {
-        display: grid;
-        grid-template-columns: 1fr 2fr;
-        margin: 20px 0;
-        font-size: 16px;
-        line-height: 44px;
+    display: grid;
+    grid-template-columns: 1fr 2fr;
+    margin: 20px 0;
+    font-size: 16px;
+    line-height: 44px;
     }
     .cellphone {
-        display: grid;
-        grid-template-columns: 33.3% 14.7% 52%;
+    display: grid;
+    grid-template-columns: 33.3% 14.7% 52%;
     }
     input::-webkit-outer-spin-button,
     input::-webkit-inner-spin-button {
-        -webkit-appearance: none;
-        margin: 0;
+    -webkit-appearance: none;
+    margin: 0;
     }
 
 
     .subscribe-area {
-        border: 1px solid #EEEEEE;
-        padding: 20px;
+    border: 1px solid #EEEEEE;
+    padding: 20px;
     }
 
 
     .publicity {
-        margin-top: 20px;
-        border-right: 1px solid #EEEEEE;
-        padding-right: 20px;
+    margin-top: 20px;
+    border-right: 1px solid #EEEEEE;
+    padding-right: 20px;
     }
 
 
     .subscribe-item {
-        display: flex;
-        width: 100%;
-        justify-content: space-between;
-        margin: 20px 0;
-        height: 34px;
-        line-height: 34px;
+    display: flex;
+    width: 100%;
+    justify-content: space-between;
+    margin: 20px 0;
+    height: 34px;
+    line-height: 34px;
     }
 
 
     .subscribe-item button {
-        border: 1px solid #EEEEEE;
-        border-radius: 10%;
-        font-size: 14px;
-        line-height: 30px;
-        height: 30px;
-        padding: 0 5px;
+    border: 1px solid #EEEEEE;
+    border-radius: 10%;
+    font-size: 14px;
+    line-height: 30px;
+    height: 30px;
+    padding: 0 5px;
     }
 
     .subscribe-item button:hover {
-        border: 1px solid black;
+    border: 1px solid black;
     }
 
     .payment {
-        margin: 20px 0;
-        border: 1px solid #EEEEEE;
-        padding: 20px;
+    margin: 20px 0;
+    border: 1px solid #EEEEEE;
+    padding: 20px;
     }
 
     .address-right-area input,
     select {
-        width: 100%;
-        height: 44px;
+    width: 100%;
+    height: 44px;
     }
     .address-right-area li:nth-child(2) div,
     .credit-card {
-        display: grid;
-        grid-template-columns: 1fr 2fr;
+    display: grid;
+    grid-template-columns: 1fr 2fr;
     }
     .btn-area {
-        display: flex;
-        justify-content: end;
-        line-height: 36px;
-        gap: 20px;
-        text-align: center;
+    display: flex;
+    justify-content: end;
+    line-height: 36px;
+    gap: 20px;
+    text-align: center;
     }
     .btn-area button {
-        border-radius: 10px;
-        border: 1px solid #EEEEEE;
-        padding: 0 10px;
-        text-align: center;
+    border-radius: 10px;
+    border: 1px solid #EEEEEE;
+    padding: 0 10px;
+    text-align: center;
     }
     .btn-area button:nth-child(2) {
-        background-color: black;
-        color: white;
+    background-color: black;
+    color: white;
     }
 
     @media screen and (width <= 1024px) {
@@ -307,58 +397,58 @@
     .introducer,
     .password,
     .third-party-registration {
-        grid-template-columns: 100%;
+    grid-template-columns: 100%;
     }
     .gender select,
     .store select {
-        height: 44px;
+    height: 44px;
     }
     .cellphone {
-        grid-template-columns: repeat(6, 1fr);
-        grid-template-rows: repeat(2, 1fr);
+    grid-template-columns: repeat(6, 1fr);
+    grid-template-rows: repeat(2, 1fr);
     }
     .cellphone label{
-        grid-column-start:1 ;
-        grid-column-end:7 ;
+    grid-column-start:1 ;
+    grid-column-end:7 ;
     }
 
     .cellphone select {
-        grid-row-start: 2;
+    grid-row-start: 2;
     }
     .cellphone input {
-        grid-row-start: 2;
-        grid-column-start: 2;
-        grid-column-end: 7;
+    grid-row-start: 2;
+    grid-column-start: 2;
+    grid-column-end: 7;
     }
     .btn-area {
-        display: grid;
-        grid-template-columns: repeat(2, 1fr);
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
     }
     .subscribe-area{
-        margin-top: 20px;
+    margin-top: 20px;
     
     }
     .publicity {
-        border-right: 0;
-        border-bottom: 1px solid #EEEEEE;
-        padding-right: 0;
+    border-right: 0;
+    border-bottom: 1px solid #EEEEEE;
+    padding-right: 0;
     }
     .address {
-        grid-template-columns: 1fr;
-        padding-bottom: 0;
+    grid-template-columns: 1fr;
+    padding-bottom: 0;
     }
     .address-right-area{
-        padding: 20px;
-        padding-top: 0;
+    padding: 20px;
+    padding-top: 0;
     }
     .credit-card{
-        grid-template-columns: 1fr;
-        padding-top: 0;
-        line-height: 44px;
+    grid-template-columns: 1fr;
+    padding-top: 0;
+    line-height: 44px;
     }
     .credit-card-right-area{
-        padding: 20px;
-        padding-top: 0;
+    padding: 20px;
+    padding-top: 0;
     }
 }
 
