99100fc43c7393ea9d9e75a81c001d46d8f60134
Yan <podoyo23@gmail.com>
2024-11-28 09:50:47 +0800

    修改class-小駝峰命名(Footer)

--- src/views/Howtobuy.vue
+++ src/views/Howtobuy.vue
@@ -1,17 +1,17 @@
 <template>
   <div>
- <div class="header-img   p-4 md:w-full lg:p-14">
+ <div class="headerImg   p-4 md:w-full lg:p-14">
   <img src="https://shoplineimg.com/53eb2bccb32b41ef6e000007/5e5f6dd91d6cc4003967222f/2160x.webp?source_format=jpg" alt="">
   <img src="https://shoplineimg.com/53eb2bccb32b41ef6e000007/66d174c9346255001f6038f0/2160x.webp?source_format=jpg" alt="">
   <img src="https://shoplineimg.com/53eb2bccb32b41ef6e000007/66d174dee488f3001353d3ac/2160x.webp?source_format=jpg" alt="">
   <img class="mb-8" src="https://shoplineimg.com/53eb2bccb32b41ef6e000007/66d174dee488f3001353d3ac/2160x.webp?source_format=jpg" alt="">
-  <span class="content-Text">&nbsp;注意：</span><br>
+  <span class="contentText">&nbsp;注意：</span><br>
   <span class=" underline">➜ 超商取貨付款金額超過 NT$3000 以上，須先完成付款才會安排包裹出貨，若未選擇『先付款』之訂單，客服人員會與您聯繫</span>
   <span class="underline">『先付款』之訂單，客服人員會與您聯繫其他付款方式，造成不便還請見諒。</span>
   <img class=" mt-8" src="https://shoplineimg.com/53eb2bccb32b41ef6e000007/5e5f769eebebf10024512155/2160x.webp?source_format=jpg" alt="">
  </div>
  <!-- 購物金使用方式 -->
- <div class="flex col header-img m-auto">
+ <div class="flex col headerImg m-auto">
   <a href="https://www.bonnyread.com.tw/pages/discount-guide"><img src="https://shoplineimg.com/53eb2bccb32b41ef6e000007/5e621a8f45a88442c681cdb5/540x.webp?source_format=jpg" alt=""></a>
   <a href="https://www.bonnyread.com.tw/pages/couponcode-guide"><img src="https://shoplineimg.com/53eb2bccb32b41ef6e000007/5e60d3adf27ac02e24268b3a/540x.webp?source_format=jpg" alt=""></a>
   <a href="https://www.bonnyread.com.tw/pages/package"><img src="https://shoplineimg.com/53eb2bccb32b41ef6e000007/5e96b305e186ea002d5548ce/540x.webp?source_format=jpg" alt=""></a>
@@ -21,10 +21,10 @@
 </template>
 
 <style scoped>
-/* .header-img {
+/* .headerImg {
   width: 940px;
 } */
-.content-Text{
+.contentText{
   font-size: 14px;
 }
 </style>
