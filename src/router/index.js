6f42d879c7c35b418df5a135a8ddb6916b4795e5
Leon Li <84223628+leonli8731@users.noreply.github.com>
2024-12-04 14:51:36 +0800

    小購物車完成版初版
    絕大部份檔案都來自12/04早上的dev，除了十個與小購物車相關的檔案之外
    目前仍有的問題:
    1.大部分小購物車相關的js尚未改造成能與數據庫聯動的樣式
    2.homeview引入的聯名商品區似乎有問題...該商品區的東西無法加入購物車。但如果把該區註解掉，改用productlist那裏的聯名商品區，反而可行
    3.小購物車尚未與大購物車聯動
    4.nav-bar已經與小購物車聯動，另建檔案nav-bar2以做測試，避免意外
    5.storeinfo改成用第五版storeinfo5
    6.smallcart請用第四版smallcart4
    7.index.js & products.js都有更新與小購物車相關的js

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,9 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import ProductList from '@/views/ProductList.vue'
-import StoreInfo4 from '@/components/StoreInfo4.vue'
+import StoreInfo5 from '@/components/StoreInfo5.vue'
 import Cart from '../views/Cart.vue'
+import UsersEdit from "@/views/UsersEdit.vue"
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
 
@@ -14,9 +15,19 @@ const router = createRouter({
     component: HomeView,
   },
   {
+    path: "/franchising",
+    name: "franchising",
+    component: () => import("../views/Franchising.vue"),
+  },
+  {
     path: '/about',
     name: 'about',
     component: () => import('../views/AboutView.vue'),
+  },
+  {
+    path: "/reviews",
+    name: "reviews",
+    component: () => import("../components/Reviews.vue"),
   },
   {
     path: '/vip',
@@ -61,7 +72,46 @@ const router = createRouter({
   {
     path: '/store-info',
     name: 'store-info',
-    component: StoreInfo4,
+    component: StoreInfo5,
+  },
+  {
+    path: "/users/edit",
+    name: "users-edit",
+    component: UsersEdit,
+  },{
+    path: "/MemberCoupons",
+    name: "MemberCoupons",
+    component: () => import("../views/MemberCoupons.vue"),
+  },
+  {
+    path: "/MemberMessage",
+    name: "MemberMessage",
+    component: () => import("../views/MemberMessage.vue"),
+  },
+  {
+    path: "/MemberOrder",
+    name: "MemberOrder",
+    component: () => import("../views/MemberOrder.vue"),
+  },
+  {
+    path: "/MemberPoint",
+    name: "MemberPoint",
+    component: () => import("../views/MemberPoint.vue"),
+  },
+  {
+    path: "/MemberShoppingGold",
+    name: "MemberShoppingGold",
+    component: () => import("../views/MemberShoppingGold.vue"),
+  },
+  {
+    path: "/MemberWishList",
+    name: "MemberWishList",
+    component: () => import("../views/MemberWishList.vue"),
+  },
+  {
+    path: "/OrderDetails",
+    name: "OrderDetails",
+    component: () => import("../views/OrderDetails.vue"),
   },
   {
     path:'/Cart',
