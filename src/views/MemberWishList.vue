b45b1ab68efec300c9c8cf82959d0d5dda1b9352
WEI\頌偉 <rocksypig@gmail.com>
2024-12-09 23:18:25 +0800

    chatbox版面更新 會員追蹤清單頁面完成，可以新增刪除， 再來要做商品詳情@click去新增資料進去 訂單詳情頁面更新動畫 新增評論頁面

--- src/views/MemberWishList.vue
+++ src/views/MemberWishList.vue
@@ -1,181 +1,239 @@
 <template>
-	<MemberNavbar />
-	<div class="wishlistMain mx-auto p-10">
-		<div
-			class="wishlistTitle grid grid-cols-12 items-center border-b pb-2 text-[12px] font-semibold"
-		>
-			<div class="col-span-2 px-[15px]">
-				<p>商品圖</p>
-			</div>
-			<div class="col-span-4 px-[15px]">
-				<p>商品資訊</p>
-			</div>
-			<div class="col-span-3 px-[15px]">
-				<p>價格</p>
-			</div>
-			<div class="col-span-2 px-[15px]">
-				<p>狀態</p>
-			</div>
-			<div class="col-span-1 px-[15px]">
-				<p>刪除</p>
-			</div>
-		</div>
-		<div
-			class="commodityList grid grid-cols-12 items-center border-b py-[15px] mx-[-10px] bg-white hover:bg-[#F5F5F5]"
-		>
-			<div class="commodityImg col-span-2 px-[15px]">
-				<img
-					class="px-[15px]"
-					src="https://picsum.photos/id/184/184/184"
-					alt=""
-				/>
-			</div>
-			<div class="commodityName col-span-4 px-[15px]">
-				<p>【限時促銷】[寶可夢/鍍白金] 立體造型吊墜 / 銀色 / 8款</p>
-				<p class="shape">款式 : 立體精靈球</p>
-			</div>
-			<div class="commodityPrice col-span-3 px-[15px] text-[12px]">NT$456</div>
-			<div class="commodityStatus col-span-2 px-[15px] text-[12px]">
-				<p>無法購買</p>
-			</div>
-			<div class="commodityDelete col-span-1 px-[15px]">
-				<p><font-awesome-icon :icon="['fas', 'trash']" /></p>
-			</div>
-		</div>
-		<div
-			class="commodityList grid grid-cols-12 items-center border-b py-[15px] mx-[-10px] bg-white hover:bg-[#F5F5F5]"
-		>
-			<div class="commodityImg col-span-2 px-[15px]">
-				<img
-					class="px-[15px]"
-					src="https://picsum.photos/id/184/184/184"
-					alt=""
-				/>
-			</div>
-			<div class="commodityName col-span-4 px-[15px]">
-				<p>【限時促銷】[寶可夢/鍍白金] 立體造型吊墜 / 銀色 / 8款</p>
-				<p class="shape">款式 : 立體精靈球</p>
-			</div>
-			<div class="commodityPrice col-span-3 px-[15px] text-[12px]">NT$456</div>
-			<div class="commodityStatus col-span-2 px-[15px] text-[12px]">
-				<i class="fa-solid fa-cart-shopping"></i>
-				<button class="btn">加入購物車</button>
-				<button class="btn my-[10px]">
-					<font-awesome-icon
-						:icon="['fas', 'bag-shopping']"
-						style="color: #fff; margin-right: 4px"
-					/>立即購買
-				</button>
-			</div>
-			<div class="commodityDelete col-span-1 px-[15px]">
-				<p><font-awesome-icon :icon="['fas', 'trash']" /></p>
-			</div>
-		</div>
-	</div>
+    <MemberNavbar />
+    <div class="wishlistMain mx-auto p-10">
+        <div
+            class="wishlistTitle grid grid-cols-12 items-center border-b pb-2 text-[12px] font-semibold"
+        >
+            <div class="col-span-2 px-[15px]">
+                <p>商品圖</p>
+            </div>
+            <div class="col-span-4 px-[15px]">
+                <p>商品資訊</p>
+            </div>
+            <div class="col-span-3 px-[15px]">
+                <p>價格</p>
+            </div>
+            <div class="col-span-2 px-[15px]">
+                <p>狀態</p>
+            </div>
+            <div class="col-span-1 px-[15px]">
+                <p>刪除</p>
+            </div>
+        </div>
+        <div
+            v-for="item in wishlists"
+            :key="item.id"
+            class="commodityList grid grid-cols-12 items-center border-b py-[15px] mx-[-10px] bg-white hover:bg-[#F5F5F5]"
+        >
+            <div class="commodityImg col-span-2 px-[15px]">
+                <img
+                    v-if="item.products.product_images.length > 0"
+                    :src="
+                        resolveImagePath(
+                            item.products.product_images[0].image_path
+                        )
+                    "
+                    :alt="
+                        item.products.product_images[0].alt_text || '商品圖片'
+                    "
+                />
+            </div>
+            <div class="commodityName col-span-4 px-[15px]">
+                <p>{{ item.products.product_name }}</p>
+                <p class="shape">
+                    {{ item.products.product_specs[0].spec_value }}
+                </p>
+            </div>
+            <div class="commodityPrice col-span-3 px-[15px] text-[12px]">
+                <p class="originalPrice">
+                    NT${{ item.products.original_price }}
+                </p>
+                <p class="salePrice">NT${{ item.products.sale_price }}</p>
+            </div>
+            <div class="commodityStatus col-span-2 px-[15px] text-[12px]">
+                <button class="btn" v-if="item.products.status == 1">
+                    加入購物車
+                </button>
+                <button class="btn mt-[10px]" v-if="item.products.status == 1">
+                    <font-awesome-icon
+                        style="color: aliceblue"
+                        class="shoppingBag h-[16px] w-[16px] mr-[2px]"
+                        :icon="['fas', 'bag-shopping']"
+                    />立即結帳
+                </button>
+                <p v-else>無法購買</p>
+            </div>
+            <div class="commodityDelete col-span-1 px-[15px]">
+                <p>
+                    <font-awesome-icon
+                        :icon="['fas', 'trash']"
+                        class="cursor-pointer"
+                        @click="removeItem(item.id)"
+                    />
+                </p>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script setup>
-import MemberNavbar from "./MemberNavbar.vue"
+import { ref, onMounted } from "vue";
+import axios from "axios";
+import MemberNavbar from "./MemberNavbar.vue";
+
+const STORAGE_KEY = "userUUID"; // 給localstroage一個變數
+const wishlists = ref([]);
+
+function resolveImagePath(path) {
+    return `http://localhost:3000${path.replace("./", "/")}`;
+}
+
+onMounted(async () => {
+    //抓UUID
+    const memberUUID = localStorage.getItem(STORAGE_KEY);
+
+    try {
+        const response = await axios.get(
+            `http://localhost:3000/wishlist/${memberUUID}`
+        );
+        if (response.data.status === "Success") {
+            wishlists.value = response.data.data;
+        } else {
+            console.error("API回傳錯誤: ", response.data.message);
+        }
+    } catch (error) {
+        console.error("無法獲取願望清單資料：", error);
+    }
+});
+const removeItem = async (id) => {
+    console.log(`刪除項目 ID: ${id}`);
+    try {
+        await axios.delete(`http://localhost:3000/wishlist/delete/${id}`);
+        wishlists.value = wishlists.value.filter((item) => item.id !== id);
+        console.log(`刪除成功: ${id}`);
+    } catch (error) {
+        console.error("刪除失敗", error);
+    }
+};
 </script>
+
 <style scoped>
 * {
-	font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
-	font-size: 14px;
-	color: #333333;
+    font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
+    font-size: 14px;
+    color: #333333;
 }
 .wishlistMain {
-	border: 1px solid #ddd;
-	max-width: 1358px;
-	border-top: 1px solid #fff;
+    border: 1px solid #ddd;
+    max-width: 1358px;
+    border-top: 1px solid #fff;
 }
 .wishlistTitle {
-	border-bottom: 1px solid #ddd;
+    border-bottom: 1px solid #ddd;
 }
 .commodityList {
-	border-bottom: 1px solid rgba(221, 221, 221, 0.5);
+    border-bottom: 1px solid rgba(221, 221, 221, 0.5);
 }
 .btn {
-	width: 160px;
-	display: flex;
-	align-items: center;
-	justify-content: center;
-	padding: 6px 12px;
-	background-color: #000;
-	color: #fff;
-	border: none;
-	border-radius: 20px;
-	font-size: 16px;
-	font-weight: bold;
-	cursor: pointer;
-	transition: background-color 0.3s ease;
+    width: 160px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    padding: 6px 12px;
+    background-color: #000;
+    color: #fff;
+    border: 1px solid #fff;
+    border-radius: 20px;
+    font-size: 16px;
+    font-weight: bold;
+    cursor: pointer;
+    transition: background-color 0.3s ease;
+}
+.btn:hover {
+    background-color: #fff;
+    color: #000;
+    border: #000 1px solid;
+}
+.btn:hover > .shoppingBag {
+    color: #000 !important;
 }
 .shape {
-	color: #80808099;
+    color: #80808099;
+    font-size: 12px;
 }
 .fa-cart-shopping {
-	display: none !important;
+    display: none !important;
 }
+.originalPrice {
+    font-size: 12px;
+    color: #ddd;
+    text-decoration: line-through;
+}
+.salePrice {
+    font-size: 12px;
+}
+
 @media (max-width: 1024px) and (min-width: 769px) {
-	.btn {
-		display: none;
-	}
-	.fa-cart-shopping {
-		display: block !important;
-	}
+    .btn {
+        display: none;
+    }
+    .fa-cart-shopping {
+        display: block !important;
+    }
 }
 @media (max-width: 768px) {
-	.wishlistMain {
-		width: 100%;
-	}
-	.wishlistTitle {
-		display: none;
-	}
-	.commodityList {
-		display: flex;
-		flex-direction: column;
-		width: 80%;
-		margin: auto;
-		padding: 0;
-		border: 1px solid #ddd;
-		padding-top: 15px;
-	}
-	.commodityList:nth-of-type(n + 2) {
-		margin-top: 20px;
-	}
-	.commodityImg {
-		padding-left: 15px;
-		padding-right: 15px;
-		margin-bottom: 25px;
-		width: 80%;
-	}
-	.commodityImg img {
-		width: 100%;
-		height: 100%;
-		object-fit: cover;
-		padding: 0;
-		margin: 0;
-	}
-	.commodityName {
-		width: 100%;
-		font-size: 14px;
-		color: #797979;
-		text-align: left;
-	}
-	.commodityPrice {
-		padding-top: 7px;
-		padding-bottom: 7px;
-		text-align: left;
-		width: 100%;
-	}
-	.commodityStatus {
-		width: 100%;
-		background-color: #efefef;
-		padding: 15px;
-		text-align: center;
-		line-height: 12px;
-	}
-	.commodityDelete {
-		display: none;
-	}
+    .wishlistMain {
+        width: 100%;
+    }
+    .wishlistTitle {
+        display: none;
+    }
+    .commodityList {
+        display: flex;
+        flex-direction: column;
+        width: 80%;
+        margin: auto;
+        padding: 0;
+        border: 1px solid #ddd;
+        padding-top: 15px;
+    }
+    .commodityList:nth-of-type(n + 2) {
+        margin-top: 20px;
+    }
+    .commodityImg {
+        padding-left: 15px;
+        padding-right: 15px;
+        margin-bottom: 25px;
+        width: 80%;
+    }
+    .commodityImg img {
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+        padding: 0;
+        margin: 0;
+    }
+    .commodityName {
+        width: 100%;
+        font-size: 14px;
+        color: #797979;
+        text-align: left;
+    }
+    .commodityPrice {
+        padding-top: 7px;
+        padding-bottom: 7px;
+        text-align: left;
+        width: 100%;
+    }
+    .commodityStatus {
+        width: 100%;
+        background-color: #efefef;
+        padding: 15px;
+        text-align: center;
+        line-height: 12px;
+    }
+    .commodityDelete {
+        display: none;
+    }
 }
 </style>
---
+++ src/views/product_review_comments.vue
@@ -0,0 +1,130 @@
+<template>
+    <div class="reviewMain mx-auto">
+        <div class="awaitingComment w-1/4">
+            <div
+                class="awaitingCommentTitle border py-[10px] pl-[15px] font-semibold text-[18px]"
+            >
+                <h4>待評價訂單</h4>
+            </div>
+            <div class="orderNumbers text-[14px]">
+                <h5 class="my-[10px]">訂單編號</h5>
+                <p class="pb-[10px]">20241123060952668</p>
+            </div>
+        </div>
+        <div class="givingComment w-3/4 pb-[15px]">
+            <div
+                class="awaitingCommentTitle border py-[10px] pl-[15px] font-semibold text-[18px]"
+            >
+                <h4>給予評價</h4>
+            </div>
+            <div class="commentContent p-[15px]">
+                <span class="text-[11px]">訂單編號</span>
+                <hr class="my-[20px]" />
+                <div class="commodityInformation flex">
+                    <div class="commodityImg px-[15px]">
+                        <img
+                            src="https://shoplineimg.com/53eb2bccb32b41ef6e000007/632435be69ad7f002b89558e/600x600f.webp?source_format=jpg"
+                            alt=""
+                        />
+                    </div>
+                    <div class="w-full">
+                        <div>
+                            <h5 class="font-semibold text-[14px]">
+                                [寶可夢/鍍18K厚金] 皮卡丘戒指 / 2色 / 4 size
+                            </h5>
+                        </div>
+                        <div class="py-[15px] text-[14px]">
+                            評價<font-awesome-icon
+                                class="mx-1"
+                                :icon="['fas', 'star']"
+                            />
+                        </div>
+                        <div class="commentInput text-[14px]">
+                            內容<br />
+                            <textarea
+                                class="h-[92px] w-full px-[12px] py-[6px]"
+                                placeholder="評價內容"
+                            ></textarea>
+                        </div>
+                    </div>
+                </div>
+                <hr class="my-[20px]" />
+                <div class="postButton text-right">
+                    <button class="postBtn">發佈</button>
+                </div>
+            </div>
+            <div></div>
+        </div>
+    </div>
+</template>
+
+<style scope>
+* {
+    font-family: "Helvetica Neue", Helvetica, Arial, sans-serif !important;
+    font-size: 14px;
+    color: #333333;
+}
+.reviewMain {
+    max-width: 1400px;
+    display: flex;
+    gap: 30px;
+}
+.awaitingCommentTitle {
+    background-color: #f5f5f5;
+    border: solid 1px #ddd;
+}
+.orderNumbers {
+    padding: 15px;
+    border: solid 1px #ddd;
+    border-top: solid 1px #fff;
+}
+.commentContent {
+    border: solid 1px #ddd;
+    border-top: solid 1px #fff;
+}
+.commodityImg {
+    max-height: 215px;
+    max-width: 215px;
+    box-sizing: content-box;
+    float: left;
+}
+.commodityImg img {
+    height: 100%;
+    width: 100%;
+    object-fit: cover;
+}
+textarea {
+    border: 1px solid #ddd;
+    resize: none;
+    border-radius: 3px;
+}
+.postBtn {
+    background-color: #000;
+    color: #fff;
+    padding: 6px 12px;
+    border: none;
+    border-radius: 10px;
+    cursor: pointer;
+    width: 28px;
+    height: 20px;
+    box-sizing: content-box;
+    font-size: 14px;
+}
+@media (max-width: 768px) {
+    .reviewMain {
+        display: flex;
+        flex-direction: column-reverse;
+        padding: 10px 15px;
+    }
+    .awaitingComment {
+        width: 100%;
+        padding: 15px;
+        padding-top: 0;
+    }
+}
+.givingComment {
+    width: 100%;
+    padding: 15px;
+    padding-top: 0;
+}
+</style>
