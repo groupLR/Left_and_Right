c79688eb861d952b0c26eccdcc896e49166888bb
33-Fang <33justworkplace@gmail.com>
2024-12-12 16:35:36 +0800

    共享購物車列表切版

---
+++ src/components/SharedCartItem.vue
@@ -0,0 +1,19 @@
+<script setup>
+</script>
+
+<template>
+  <div class="w-full box-border border-2 border-solid border-gray-100 rounded my-2 shadow-sm">
+    <div class=" w-full box-border flex justify-between p-4">
+      <div class=" flex flex-row mx-1 items-center">
+        <span>你與</span>
+        <span>xxx</span>
+        <span>與其他三個人</span>
+        <span>的共享購物車</span>
+      </div>
+      <div>
+        <button class="border px-4 py-1 rounded bg-black text-white hover:bg-emerald-300">查看</button>
+        <button class="ml-6"><i class="fa-solid fa-trash text-gray-400 hover:text-red-400"></i></button>
+      </div>
+    </div>
+  </div>
+</template>
---
+++ src/views/SharedCartList.vue
@@ -0,0 +1,17 @@
+<script setup>
+import SharedCartItem from '@/components/SharedCartItem.vue';
+</script>
+
+<template>
+  <div class=" bg-gray-100 m-5 mb-[-20px] p-2 flex items-center">
+    <i class="fa-solid fa-people-carry-box ml-4"></i>
+    <h1 class=" text-lg font-bold ml-2">共享購物車</h1>
+  </div>
+  <div class=" p-4 flex flex-col border-2 border-solid border-gray-100 rounded-b m-5">
+    <SharedCartItem />
+    <SharedCartItem />
+  </div>
+</template>
+
+<style scoped>
+</style>
