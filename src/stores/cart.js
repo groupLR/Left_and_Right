d6fcac1853a361debf19550c20e01f277db39e57
33-Fang <33justworkplace@gmail.com>
2024-12-18 15:08:44 +0800

    refactor: #63 移除非必要檔案與內容

--- src/stores/cart.js
+++ src/stores/cart.js
@@ -1,28 +1,22 @@
-import { defineStore } from "pinia";
-import { ref, computed } from "vue";
-import axios from 'axios';
+import { defineStore } from "pinia"
+import axios from "axios"
 
-export const useCartStore = defineStore('cart', () => {
-  const API_URL = 'http://localhost:3300'
-
+export const useCartStore = defineStore("cart", () => {
   // 新增商品到購物車 API
   const addProduct = async (productId) => {
     try {
-      const userId = localStorage.getItem('UID')
-      const {data} = await axios.post(`${API_URL}/cart/cartInsert`,
-        {
-          user_id: userId,
-          product_id: productId,
-          quantity: 1,
-        }
-      )
-      return data      
+      const userId = localStorage.getItem("UID")
+      const { data } = await axios.post(`${process.env.API_URL}/cart/cartInsert`, {
+        user_id: userId,
+        product_id: productId,
+        quantity: 1,
+      })
+      return data
     } catch (err) {
-      console.log("加入購物車失敗", err);
+      console.log("加入購物車失敗", err)
       return err
     }
   }
-
 
   return {
     addProduct,
