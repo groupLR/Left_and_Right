171299e355932ad4282859bfc21d1a04795894fc
33-Fang <33justworkplace@gmail.com>
2025-01-06 12:42:21 +0800

    refactor: 移除 console.log

--- src/views/Cart.vue
+++ src/views/Cart.vue
@@ -104,7 +104,6 @@ const fetchCartItems = async () => {
         },
       })
       products.value = response.data // 將 API 返回的資料存入 products
-      console.log(products.value)
     } catch (error) {
       console.error("獲取資料失敗:", error)
     }
